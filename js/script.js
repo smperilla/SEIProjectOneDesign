f01adfd82cc3e242bccbfa7d86774c15a147a4ae
sperilla <smperilla@gmail.com>
2023-07-05 19:37:59 -0400

    search bar console logs article related to keyword

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 
 
+const API_KEY = 'f39cd624d93c451fbf1668468abc0d11';
+const BASE_URL = 'https://newsapi.org/v2/everything?q=';
+
 let userInput;
 
 const $input = $('input[type="text"]');
@@ -10,5 +13,10 @@ $('form').on('submit',handleGetData)
 function handleGetData(event) {
     event.preventDefault();
     userInput = $input.val();
-    console.log($input.val());
+    $.ajax(BASE_URL + userInput + '&apiKey=' + API_KEY)
+    .then((data) => {
+        console.log(data.articles[0]);
+    });
+
+
 }
